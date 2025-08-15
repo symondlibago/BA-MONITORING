c06ac14aa2dc637a8f1c889b40e270fd1e854431
symondlibago <libago.symond1@gmail.com>
2025-08-15 14:45:42 +0800

    deploy

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -28,6 +28,12 @@ use App\Http\Controllers\RFIQueryController;
 |
 */
 
+
+Route::options('{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
+
+
 // Public routes
 Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.urnjcpgt21o"
+    "revision": "0.up1tim9grvo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
