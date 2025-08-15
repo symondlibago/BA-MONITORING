73d94c6ec086a770e481fabdbb64ef654f3500c8
symondlibago <libago.symond1@gmail.com>
2025-08-15 13:12:56 +0800

    deploy

--- frontend/src/components/Config.jsx
+++ frontend/src/components/Config.jsx
@@ -1,4 +1,2 @@
-const API_BASE_URL = 'http://localhost:8000/api';
-// const API_BASE_URL = 'http://192.168.1.132:8000/api';
-
+const API_BASE_URL = 'https://ba-monitoring-production.up.railway.app/api';
 export default API_BASE_URL;
